ad66ef50efc2dc5458aa9cba21155022acab4d48
Ignacio <martin.ignacio@hotmail.com>
2016-09-05 12:18:05 -0300

    finished FR-26 and FR-27 mobile version

--- Gruntfile.js
+++ Gruntfile.js
@@ -81,7 +81,7 @@ module.exports = function (grunt) {
         tasks: [
           'sass:ui',
           'postcss',
-          'sassdoc'
+          // 'sassdoc'
         ],
         options: {
           spawn: false
